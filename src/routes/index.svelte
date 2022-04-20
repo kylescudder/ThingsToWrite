2cac34c9f7107babd65943ffb7e3775000b3073a
Kyle Scudder <kyle.s@paloma.co.uk>
2022-04-20 23:01:21 +0100

    Login form given bg-color

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -60,7 +60,7 @@ import { onMount } from 'svelte';
 </svelte:head>
 {#if !user}
 <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-  <div class="max-w-md w-full space-y-8">
+  <div class="max-w-md w-full space-y-8 p-8 bg-gray-300 rounded-lg">
     <div>
       <img class="mx-auto h-12 w-auto" src={markdownLogo} alt="Markdown Logo">
       <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">ThingsToWrite</h2>
