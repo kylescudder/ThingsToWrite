3e7688a97d0fdc8ea50e2b76865118e63ae6a621
kylescudder <74150974+kylescudder@users.noreply.github.com>
2022-04-21 09:15:45 +0100

    Wrote auth flow playwright tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,14 @@
 import { expect, test } from '@playwright/test';
 
-test('about page has expected h1', async ({ page }) => {
-	await page.goto('/about');
-	expect(await page.textContent('h1')).toBe('About this app');
+test('Login form has expected p tag with strap line', async ({ page }) => {
+	await page.goto('/');
+	expect(await page.textContent('p')).toBe('A convenient place to store all of your markdown notes.');
+});
+
+test('Login/Lougout flow', async ({ page }) => {
+	await page.goto('/');
+	expect(await page.locator('text=Login with GitHub').click());
+	setTimeout(() => {
+		expect(page.locator('text=Logout').click());
+	}, 2000);
 });
