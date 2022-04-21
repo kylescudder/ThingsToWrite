c8e87e84a6049c39d2f07a62be47f12fae984a79
kylescudder <74150974+kylescudder@users.noreply.github.com>
2022-04-21 13:36:11 +0100

    Moved workspace, workspacelist and editor to components

---
+++ src/components/workspace.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import WorkspaceList from '../components/workspacelist.svelte';
+	import Editor from '../components/editor.svelte';
+	export let userName: string
+	import Loading from './Loading.svelte'
+	export let isLoading = false
+
+	isLoading = true
+	setTimeout(() => {
+		isLoading = false
+	}, 300);
+</script>
+{#if isLoading}
+ <Loading/>
+{:else}
+	<div class="grid grid-cols-12 h-full w-full">
+		<WorkspaceList userName={userName} />
+		<div class="col-span-2" />
+		<div class="col-span-8">
+			<Editor />
+		</div>
+	</div>
+{/if}
